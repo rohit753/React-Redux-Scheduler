0a7ef4b93884a73fc1d9d91bd64d3e6170bc3e1a
Rohit Kumar Sahu <rohit753kumar@gmail.com>
2022-02-20 01:28:23 +0530

    removing unused var

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from "react";
+import React, { useState } from "react";
 // import { useSelector} from 'react-redux'
 import { Link, useParams } from 'react-router-dom';
 import { useDispatch } from "react-redux";
